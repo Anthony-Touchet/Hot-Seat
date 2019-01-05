604b10c031d68294c64d62a56c2934197b9678bc
Anthony Touchet <anthonyjtouchet@gmail.com>
2019-01-05 09:56:04 -0600

    Buttons Adding to List
    Each button can add its associated number to the list of players in the
    lotto and can remove them if they are already in the lotto

---
+++ Assets/Scripts/Hot_Seat_Lottery.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hot_Seat_Lottery : MonoBehaviour
+{
+    public List<GameObject> buttons;
+    public List<string> participants = new List<string>();
+
+    // Start is called before the first frame update
+    public void StartLottery()
+    {
+        
+    }
+
+    public void ButtonPress(GameObject go)
+    {
+        var text = go.GetComponentInChildren(typeof(Text)) as Text;
+        var lottoNum = text.text;
+
+        if (participants.Contains(lottoNum))
+        {
+            participants.Remove(lottoNum);
+            return;
+        }
+            
+        participants.Add(lottoNum);
+    }
+}
