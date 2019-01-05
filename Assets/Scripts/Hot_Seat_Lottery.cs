dd05fc6af527025e54f2d1e47fad58d72bb4373e
Anthony Touchet <anthonyjtouchet@gmail.com>
2019-01-05 11:11:44 -0600

    Buttons changed to Toggles
    Toggles allowed easier control of input and easier control of colors

--- Assets/Scripts/Hot_Seat_Lottery.cs
+++ Assets/Scripts/Hot_Seat_Lottery.cs
@@ -14,17 +14,41 @@ public class Hot_Seat_Lottery : MonoBehaviour
         
     }
 
-    public void ButtonPress(GameObject go)
+    public void TogglePress(GameObject go)
     {
-        var text = go.GetComponentInChildren(typeof(Text)) as Text;
-        var lottoNum = text.text;
+        ColorBlock newColor = ColorBlock.defaultColorBlock;
+        var text = go.GetComponentInChildren<Text>();
+        Toggle toggle = go.GetComponent<Toggle>();
+        string lottoNum = text.text;
 
         if (participants.Contains(lottoNum))
         {
             participants.Remove(lottoNum);
+
+            if (lottoNum.Contains("r"))
+            {               
+                newColor.highlightedColor = new Color(1, .2f, .2f);
+                newColor.normalColor = new Color(1, .2f, .2f);
+            }
+            else if (lottoNum.Contains("g"))
+            {
+                newColor.highlightedColor = new Color(0, 1, 0);
+                newColor.normalColor = new Color(0, 1, 0);
+            }
+            else if (lottoNum.Contains("b"))
+            {
+                newColor.highlightedColor = new Color(0,1,1);
+                newColor.normalColor = new Color(0, 1, 1);
+            }
+            toggle.colors = newColor;
             return;
         }
-            
+
         participants.Add(lottoNum);
+
+        newColor.highlightedColor = Color.yellow;
+        newColor.normalColor = Color.yellow;
+        toggle.colors = newColor;
+        return;
     }
 }
